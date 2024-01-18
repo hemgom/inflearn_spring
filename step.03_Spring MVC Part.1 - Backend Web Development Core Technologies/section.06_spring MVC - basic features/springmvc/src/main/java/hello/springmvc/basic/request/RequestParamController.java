8e12d8a2c91872d5baa9e5e25788d1b241ac10a6
hemgom <reborn.april23@gmail.com>
2024-01-19 08:26:00 +0900

    studied step.03-06-08

--- step.03_Spring MVC Part.1 - Backend Web Development Core Technologies/section.06_spring MVC - basic features/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ step.03_Spring MVC Part.1 - Backend Web Development Core Technologies/section.06_spring MVC - basic features/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,9 +1,11 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -78,6 +80,36 @@ public class RequestParamController {
     public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
         // 파라미터를 MultiValueMap으로도 조회 가능
         log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        // @ModelAttribute 사용시 아래의 코드를 생략할 수 있다
+        //HelloData helloData = new HelloData();
+        //helloData.setUsername(username);
+        //helloData.setAge(age);
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        log.info("helloData={}", helloData);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    /**
+     * 스프링은 자동으로 다음의 규칙을 적용한다
+     * String, int 등의 단순 타입 = @RequestParam
+     * 개발자가 생성한 객체 = @ModelAttribute
+     * 단, argument resolver로 지정된 타입들은 규칙에서 제외된다. (ex_ HttpServletRequest 등)
+     */
+    public String modelAttributeV2(HelloData helloData) {
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        log.info("helloData={}", helloData);
+
         return "ok";
     }
 }
