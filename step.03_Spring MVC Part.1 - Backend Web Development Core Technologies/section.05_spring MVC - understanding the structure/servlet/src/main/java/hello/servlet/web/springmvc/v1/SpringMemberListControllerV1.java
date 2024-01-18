36d48a1825030cbbbe7d9e75f388f4df6e347b6d
hemgom <reborn.april23@gmail.com>
2024-01-19 01:21:00 +0900

    studied step.03-05-05

--- step.03_Spring MVC Part.1 - Backend Web Development Core Technologies/section.05_spring MVC - understanding the structure/servlet/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
+++ step.03_Spring MVC Part.1 - Backend Web Development Core Technologies/section.05_spring MVC - understanding the structure/servlet/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -2,8 +2,6 @@ package hello.servlet.web.springmvc.v1;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,7 +13,7 @@ public class SpringMemberListControllerV1 {
     MemberRepository memberRepository = MemberRepository.getInstance();
 
     @RequestMapping("/springmvc/v1/members")
-    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+    public ModelAndView process() {
         List<Member> members = memberRepository.findAll();
 
         ModelAndView mv = new ModelAndView("members");
---
+++ step.03_Spring MVC Part.1 - Backend Web Development Core Technologies/section.05_spring MVC - understanding the structure/servlet/src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -0,0 +1,45 @@
+package hello.servlet.web.springmvc.v2;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v2/members") // 하위 메서드의 url과 합쳐짐, 중복되는 url 일부를 정리할 수 있음
+public class SpringMemberControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/new-form") //요청 정보 매핑, url 호출시 메서드가 호출 됨
+    public ModelAndView newForm() {
+        return new ModelAndView("new-form");
+    }
+
+    @RequestMapping("/save")
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelAndView mv = new ModelAndView("save-result");
+        mv.addObject("member", member);
+        return mv;
+    }
+
+    @RequestMapping
+    public ModelAndView members() {
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+        return mv;
+    }
+}
