4a11b33abfc8dd1df86095e5a955798c416e3caf
hemgom <reborn.april23@gmail.com>
2024-01-19 03:48:35 +0900

    studied step.03-06-03

---
+++ step.03_Spring MVC Part.1 - Backend Web Development Core Technologies/section.06_spring MVC - basic features/springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,130 @@
+package hello.springmvc.basic.requestmapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MappingController {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/hello-basic") // 해당 url이 오면 아래의 메서드를 실행하도록 매핑, url은 다중 설정 가능
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    // HTTP 메서드 : GET, POST, HEAD, PUT, PATCH, DELETE
+    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET) // 특정 HTTP 메서드 요청만 허용
+    public String mappingGetV1() {
+        log.info("mappingGetV1");
+        return "ok";
+    }
+
+    /**
+     * 편리한 축약 애노테이션 (코드보기)
+     * @GetMapping
+     * @PostMapping
+     * @PutMapping
+     * @DeleteMapping
+     * @PatchMapping
+     */
+    @GetMapping("/mapping-get-v2")
+    public String mappingGetV2() {
+        log.info("mapping-get-v2");
+        return "ok";
+    }
+
+    /**
+     * PathVariable 사용
+     * 변수명이 같으면 생략 가능
+     * @PathVariable("userId") String userId -> @PathVariable userId
+     */
+    @GetMapping("/mapping/{userId}") // 경로 변수 자주 사용됨
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath userId={}", data);
+        return "ok";
+    }
+
+    /**
+     * PathVariable 사용 다중
+     */
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
+    /**
+     * 파라미터로 추가 매핑
+     * params="mode",
+     * params="!mode"
+     * params="mode=debug"
+     * params="mode!=debug" (! = )
+     * params = {"mode=debug","data=good"}
+     */
+    @GetMapping(value = "/mapping-param", params = "mode=debug") // 특정 파라미터 정보(mode=debug)가 있어야 함
+    public String mappingParam() {
+        log.info("mappingParam");
+        return "ok";
+    }
+
+    /**
+     * 특정 헤더로 추가 매핑
+     * headers="mode",
+     * headers="!mode"
+     * headers="mode=debug"
+     * headers="mode!=debug" (! = )
+     */
+    @GetMapping(value = "/mapping-header", headers = "mode=debug") // Headers에 key(mode), value(debug)가 있어야 함
+    public String mappingHeader() {
+        log.info("mappingHeader");
+        return "ok";
+    }
+
+    /**
+     * Content-Type 헤더 기반 추가 매핑 Media Type
+     * consumes="application/json"
+     * consumes="!application/json"
+     * consumes="application/*"
+     * consumes="*\/*"
+     * MediaType.APPLICATION_JSON_VALUE
+     */
+    @PostMapping(value = "/mapping-consume", consumes = "application/json") // 미디어 타입이 json 이어야만 함
+    /**
+     * 사용 예시
+     * consumes = "text/plain"
+     * consumes = {"text/plain", "application/*"}
+     * consumes = MediaType.TEXT_PLAIN_VALUE
+     */
+    public String mappingConsumes() {
+        log.info("mappingConsumes");
+        return "ok";
+    }
+
+    /**
+     * Accept 헤더 기반 Media Type
+     * produces = "text/html"
+     * produces = "!text/html"
+     * produces = "text/*"
+     * produces = "*\/*"
+     */
+    @PostMapping(value = "/mapping-produce", produces = "text/html") // 미디어 타입이 text/html 이어야만 함
+    /**
+     * 사용 예시
+     * produces = "text/plain"
+     * produces = {"text/plain", "application/*"}
+     * produces = MediaType.TEXT_PLAIN_VALUE
+     * produces = "text/plain;charset=UTF-8"
+     */
+    public String mappingProduces() {
+        log.info("mappingProduces");
+        return "ok";
+    }
+}
