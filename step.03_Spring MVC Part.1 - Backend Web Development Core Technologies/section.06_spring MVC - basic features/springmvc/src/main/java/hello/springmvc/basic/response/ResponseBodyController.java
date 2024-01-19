f29d1b63f54bdd736faaeae2fac8c240091884cc
hemgom <reborn.april23@gmail.com>
2024-01-19 17:23:36 +0900

    studied step.03-06-12

---
+++ step.03_Spring MVC Part.1 - Backend Web Development Core Technologies/section.06_spring MVC - basic features/springmvc/src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -0,0 +1,56 @@
+package hello.springmvc.basic.response;
+
+import hello.springmvc.basic.HelloData;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+//@ResponseBody    // 클래스 단위에 적용시 모든 메서드에 적용
+//@RestController  // @Controller + @ResponseBody 인 편의성을 위한 애노테이션
+                   // Rest API(HTTP API)를 생성하는 컨트롤러에 @ResponseBody 자동 적용
+public class ResponseBodyController {
+
+    @GetMapping("/response-body-string-v1")
+    public void responseBodyV1(HttpServletResponse response) throws IOException {
+        response.getWriter().write("ok");
+    }
+
+    @GetMapping("/response-body-string-v2")
+    public ResponseEntity<String> responseBodyV2() {
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+    @GetMapping("/response-body-string-v3")
+    public String responseBodyV3() {
+        return "ok";
+    }
+
+    @GetMapping("/response-body-json-v1")
+    public ResponseEntity<HelloData> responseBodyJsonV1() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+
+        return new ResponseEntity<>(helloData, HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @GetMapping("/response-body-json-v2")
+    public HelloData responseBodyJsonV2() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+
+        return helloData;
+    }
+}
--- step.03_Spring MVC Part.1 - Backend Web Development Core Technologies/section.06_spring MVC - basic features/springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
+++ step.03_Spring MVC Part.1 - Backend Web Development Core Technologies/section.06_spring MVC - basic features/springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -3,7 +3,6 @@ package hello.springmvc.basic.response;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
