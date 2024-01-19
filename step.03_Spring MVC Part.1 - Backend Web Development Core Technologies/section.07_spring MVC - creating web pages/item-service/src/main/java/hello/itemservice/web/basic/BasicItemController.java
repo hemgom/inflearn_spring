62f0796ead08110a1bb53bf29a94bd9a3e7639e6
hemgom <reborn.april23@gmail.com>
2024-01-20 00:02:46 +0900

    studied step.03-07-08

--- step.03_Spring MVC Part.1 - Backend Web Development Core Technologies/section.07_spring MVC - creating web pages/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ step.03_Spring MVC Part.1 - Backend Web Development Core Technologies/section.07_spring MVC - creating web pages/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,11 +7,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Queue;
 
 @Controller
 @RequestMapping("/basic/items")
@@ -39,9 +42,54 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
+    //@PostMapping("/add") // 밑의 다른 버전의 메서드와 충돌우려로 주석처리
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model) {
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    //@PostMapping("/add") // 밑의 다른 버전의 메서드와 충돌우려로 주석처리
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+
+        itemRepository.save(item);
+
+        // @ModelAttribute("item")처럼 이름 지정하면 자동 추가가 되기 때문에 아래의 코드를 생략가능함
+        // 단, 화면(view)에서 사용하는 이름과 같아야 함
+        //model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    //@PostMapping("/add") // 밑의 다른 버전의 메서드와 충돌우려로 주석처리
+    public String addItemV3(@ModelAttribute Item item) {
+
+        itemRepository.save(item);
+
+        // 이 경우 기본적으로 객체의 타입명의 첫 글자를 소문자로 바꿔 이름으로 지정함, Item -> item
+        //model.attribute("item", item);
+
+        return "basic/item";
+    }
+
+    // 줄임의 최종형
     @PostMapping("/add")
-    public String save() {
-        return "basic/addForm";
+    public String addItemV4(Item item) {
+
+        itemRepository.save(item);
+
+        return "basic/item";
     }
 
     // 테스트용 데이터 임의 추가
