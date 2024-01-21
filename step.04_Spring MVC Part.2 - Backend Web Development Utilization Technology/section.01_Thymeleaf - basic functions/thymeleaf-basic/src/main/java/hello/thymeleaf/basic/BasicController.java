e9790e955b82be9d62f44cb5de90f55ef140dabd
hemgom <reborn.april23@gmail.com>
2024-01-22 00:02:41 +0900

    studied step.04-01-06

--- step.04_Spring MVC Part.2 - Backend Web Development Utilization Technology/section.01_Thymeleaf - basic functions/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ step.04_Spring MVC Part.2 - Backend Web Development Utilization Technology/section.01_Thymeleaf - basic functions/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -67,6 +68,12 @@ public class BasicController {
         public String hello(String data) {
             return "Hello" + data;
         }
+    }
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
     }
 
     @Data
