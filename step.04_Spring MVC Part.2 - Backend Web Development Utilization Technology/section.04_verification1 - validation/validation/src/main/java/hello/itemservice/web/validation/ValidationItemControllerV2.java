2fd6277cb25e6e3fa0e73dd7a9ab7dcaffe4c96e
hemgom <reborn.april23@gmail.com>
2024-01-24 07:59:17 +0900

    studied step.04-04-13

--- step.04_Spring MVC Part.2 - Backend Web Development Utilization Technology/section.04_verification1 - validation/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ step.04_Spring MVC Part.2 - Backend Web Development Utilization Technology/section.04_verification1 - validation/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -10,6 +10,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -176,9 +177,14 @@ public class ValidationItemControllerV2 {
         log.info("target={}", bindingResult.getTarget());
 
         //검증 로직
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "itemName", "required"); // 단순한 기능만 제공함(ValidationUtils)
+        /* 윗줄의 코드와 같은 기능을 함 (윗줄코드 = 아래 주석 코드의 한줄 줄임 코드)
         if (!StringUtils.hasText(item.getItemName())) {
             bindingResult.rejectValue("itemName", "required");
         }
+        */
+
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
             bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
         }
