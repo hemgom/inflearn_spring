e2b78bc1fde413f4e07d56658b90c0cba46d7240
hemgom <reborn.april23@gmail.com>
2024-01-25 01:27:33 +0900

    studied step.04-05-02

--- step.04_Spring MVC Part.2 - Backend Web Development Utilization Technology/section.05_verification2 - Bean validation/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ step.04_Spring MVC Part.2 - Backend Web Development Utilization Technology/section.05_verification2 - Bean validation/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,26 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class Item {
 
     private Long id;
+
+    @NotBlank   // 빈값 + 공백만 있는 경우를 허용하지 않음
     private String itemName;
+
+    @NotNull    // null 값을 허용하지 않음
+    @Range(min = 1000, max = 1000000)   // 지정한 최소, 최대 값 범위 안의 값만 허용
     private Integer price;
+
+    @NotNull
+    @Max(9999)  // 지정한 최대 값까지만 허용
     private Integer quantity;
 
     public Item() {
