33762c75ab4552a0394e67586746a91ab697f6fa
hemgom <reborn.april23@gmail.com>
2024-01-25 21:39:19 +0900

    studied stpe.04-06-06

--- step.04_Spring MVC Part.2 - Backend Web Development Utilization Technology/section.06_login processing1 - cookies, sessions/login/src/main/java/hello/login/web/login/LoginController.java
+++ step.04_Spring MVC Part.2 - Backend Web Development Utilization Technology/section.06_login processing1 - cookies, sessions/login/src/main/java/hello/login/web/login/LoginController.java
@@ -10,11 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
-@RequiredArgsConstructor
 @Controller
+@RequiredArgsConstructor
 public class LoginController {
 
     private final LoginService loginService;
@@ -25,7 +27,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult) {
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
@@ -37,8 +39,24 @@ public class LoginController {
             return "login/loginForm";
         }
 
-        // 로그인 성공 TODO
+        // 로그인 성공 처리
+
+        //쿠키에 시간 정보를 주지 않을 경우 세션 쿠키(브라우저 종료시까지 유지)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
 
         return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private static void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
