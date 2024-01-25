e1b3c3abaa06bea1d374cc226b371f946e464439
hemgom <reborn.april23@gmail.com>
2024-01-26 01:14:45 +0900

    studied step.04-07-01

---
+++ step.04_Spring MVC Part.2 - Backend Web Development Utilization Technology/section.07_login processing2 - filter, interceptor/login/src/main/java/hello/login/domain/member/MemberRepository.java
@@ -0,0 +1,54 @@
+package hello.login.domain.member;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Slf4j
+@Repository
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();   //static 사용
+    private static long sequence = 0L;  //static 사용
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        log.info("save:member={}", member);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public Optional<Member> findByLoginId(String loginId) {
+        /*  기본 문법
+        List<Member> all = findAll();
+        for (Member m : all) {
+            if (m.getLoginId().equals(loginId)) {
+                return Optional.of(m);
+            }
+        }
+        return Optional.empty();
+        */
+
+        // 스트림(+람다) 활용
+        return findAll().stream()
+                .filter(m -> m.getLoginId().equals(loginId))
+                .findFirst();
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
