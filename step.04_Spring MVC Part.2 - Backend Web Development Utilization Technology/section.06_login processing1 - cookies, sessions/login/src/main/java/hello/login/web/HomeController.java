91b7a4541274aab51fc137111a77a6aa8ab950b6
hemgom <reborn.april23@gmail.com>
2024-01-25 23:49:01 +0900

    studied step.04-06-11

--- step.04_Spring MVC Part.2 - Backend Web Development Utilization Technology/section.06_login processing1 - cookies, sessions/login/src/main/java/hello/login/web/HomeController.java
+++ step.04_Spring MVC Part.2 - Backend Web Development Utilization Technology/section.06_login processing1 - cookies, sessions/login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionConst;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -42,7 +44,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+    //@GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model) {
 
         // 세션 관리자에 저장된 회원 정보 조회
@@ -54,6 +56,26 @@ public class HomeController {
         }
 
         model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model) {
+
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "home";
+        }
+
+        Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        // 세션에 회원 데이터가 없으면 `home`으로
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션 유지시 로그인 홈으로 이동
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 }
