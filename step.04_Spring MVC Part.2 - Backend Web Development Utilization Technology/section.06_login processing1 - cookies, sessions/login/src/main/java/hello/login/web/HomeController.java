c8054c2a12d1f371af21904d6fc041c3e636b69c
hemgom <reborn.april23@gmail.com>
2024-01-25 23:54:59 +0900

    studied step.04-06-12

--- step.04_Spring MVC Part.2 - Backend Web Development Utilization Technology/section.06_login processing1 - cookies, sessions/login/src/main/java/hello/login/web/HomeController.java
+++ step.04_Spring MVC Part.2 - Backend Web Development Utilization Technology/section.06_login processing1 - cookies, sessions/login/src/main/java/hello/login/web/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -59,7 +60,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+    //@GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model) {
 
         HttpSession session = request.getSession(false);
@@ -68,6 +69,20 @@ public class HomeController {
         }
 
         Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        // 세션에 회원 데이터가 없으면 `home`으로
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션 유지시 로그인 홈으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3Spring(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
 
         // 세션에 회원 데이터가 없으면 `home`으로
         if (loginMember == null) {
