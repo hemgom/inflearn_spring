2b5110ea4aad7494a9d4cb0dabac7352de53eaea
hemgom <reborn.april23@gmail.com>
2024-01-27 21:41:15 +0900

    studied step.04-10-05

--- step.04_Spring MVC Part.2 - Backend Web Development Utilization Technology/section.10_Spring type converter/typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
+++ step.04_Spring MVC Part.2 - Backend Web Development Utilization Technology/section.10_Spring type converter/typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +20,13 @@ public class HelloController {
     @GetMapping("/hello-v2")
     public String helloV2(@RequestParam(name = "data") Integer data) {
         System.out.println("data = " + data);
+        return "ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam(name = "ipPort") IpPort ipPort) {
+        System.out.println("ipPort IP= " + ipPort.getIp());
+        System.out.println("ipPort PORT= " + ipPort.getPort());
         return "ok";
     }
 }
