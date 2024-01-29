8c0b19cd89bbbc5d12b08a6f047bc2ab05d657c1
hemgom <reborn.april23@gmail.com>
2024-01-30 02:21:30 +0900

    studied step.05-01-07

--- step.05_Spring DB Part.1 - Core Principles of Data Access/section.01_Understanding JDBC/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ step.05_Spring DB Part.1 - Core Principles of Data Access/section.01_Understanding JDBC/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,12 +1,14 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
@@ -14,7 +16,15 @@ class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         //save
-        Member member = new Member("memberV1", 40000);
+        Member member = new Member("memberV4", 40000);
         repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        log.info("member == findMember {}", member == findMember);  // 사실 다른 인스턴스라 서로 다르기에 false 가 출력
+        // @Data 가 해당 객체의 모든 필드를 사용하도록 `equals()`를 오버라이딩 해서 true 출력
+        log.info("member equals findMember {}", member.equals(findMember));
+        assertThat(findMember).isEqualTo(member);
     }
 }
