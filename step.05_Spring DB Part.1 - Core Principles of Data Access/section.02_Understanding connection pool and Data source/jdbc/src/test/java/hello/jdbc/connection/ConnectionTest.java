e296772a9cc2c8d1e9582b7d1a3acefa894fe53f
hemgom <reborn.april23@gmail.com>
2024-01-30 04:27:16 +0900

    studied step.05-02-04

--- step.05_Spring DB Part.1 - Core Principles of Data Access/section.02_Understanding connection pool and Data source/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ step.05_Spring DB Part.1 - Core Principles of Data Access/section.02_Understanding connection pool and Data source/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -30,6 +31,23 @@ public class ConnectionTest {
         //DriverManagerDataSource -> 항상 새로운 커넥션을 획득함
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         useDtaSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션 풀링: HikariProxyConnection(Proxy) -> JdbcConnection(Target)
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDtaSource(dataSource);
+        
+        // 커넥션 풀에서 커넥션 생성시 애플리케이션 속도에 영향을 주지 않기 위해 별도의 쓰레드에서 작동함 -> 테스트가 먼저 종료되어 버림
+        // 이렇게 대기시간을 주어야 쓰레드 풀에 커넥션이 생성되는 로그를 확인 할 수 있음
+        Thread.sleep(1000);
     }
 
     private void useDtaSource(DataSource dataSource) throws SQLException {
