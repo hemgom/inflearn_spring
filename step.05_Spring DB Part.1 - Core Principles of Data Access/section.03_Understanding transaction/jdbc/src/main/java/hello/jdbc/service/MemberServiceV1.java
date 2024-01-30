91b7d1bf7d188e12d2ebdb436a041fe4321a3762
hemgom <reborn.april23@gmail.com>
2024-01-31 01:49:20 +0900

    studied step.05-03-11

--- step.05_Spring DB Part.1 - Core Principles of Data Access/section.03_Understanding transaction/jdbc/src/main/java/hello/jdbc/service/MemberServiceV1.java
+++ step.05_Spring DB Part.1 - Core Principles of Data Access/section.03_Understanding transaction/jdbc/src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -9,16 +9,16 @@ import java.sql.SQLException;
 @RequiredArgsConstructor
 public class MemberServiceV1 {
 
-    private final MemberRepositoryV1 memberRepositoryV1;
+    private final MemberRepositoryV1 memberRepository;
 
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
 
-        Member fromMember = memberRepositoryV1.findById(fromId);
-        Member toMember = memberRepositoryV1.findById(toId);
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
 
-        memberRepositoryV1.update(fromId, fromMember.getMoney() - money);
+        memberRepository.update(fromId, fromMember.getMoney() - money);
         validation(toMember);
-        memberRepositoryV1.update(toId, toMember.getMoney() + money);
+        memberRepository.update(toId, toMember.getMoney() + money);
 
     }
 
