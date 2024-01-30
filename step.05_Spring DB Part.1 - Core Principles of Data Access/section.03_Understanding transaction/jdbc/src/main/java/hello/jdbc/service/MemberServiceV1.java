ef1ac2ade168f69eca40c563b884ee1c683ac3e9
hemgom <reborn.april23@gmail.com>
2024-01-31 01:09:48 +0900

    studied step.05-03-10

---
+++ step.05_Spring DB Part.1 - Core Principles of Data Access/section.03_Understanding transaction/jdbc/src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -0,0 +1,30 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+
+    private final MemberRepositoryV1 memberRepositoryV1;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+        Member fromMember = memberRepositoryV1.findById(fromId);
+        Member toMember = memberRepositoryV1.findById(toId);
+
+        memberRepositoryV1.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepositoryV1.update(toId, toMember.getMoney() + money);
+
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 예외 발생");
+        }
+    }
+}
