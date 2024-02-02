66c79c1f3ba6d83d053fa338e19ac42498f5c775
hemgom <reborn.april23@gmail.com>
2024-02-02 23:32:56 +0900

    studied step.06-03-05

--- step.06_Spring DB Part.2 - Data Access Utilization Technology/section.03_Data Access Technology - Testing/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ step.06_Spring DB Part.2 - Data Access Utilization Technology/section.03_Data Access Technology - Testing/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,14 +2,19 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 
 //@Import(MemoryConfig.class)	// `MemoryConfig` 를 설정 파일로 사용
+@Slf4j
 @Import(JdbcTemplateV3Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
@@ -24,9 +29,20 @@ public class ItemServiceApplication {
 	 * 테스트에서 실행 -> `test` 프로필 동작
 	 */
 	@Bean
-	@Profile("local")	// 특정 프로필의 경우만 스프링 빈을 등록 -> `local`이라는 이름이 사용되는 경우만 `testDataInit`라는 스프링 빈을 등록함
+	@Profile("local")    // 특정 프로필의 경우만 스프링 빈을 등록 -> `local`이라는 이름이 사용되는 경우만 `testDataInit`라는 스프링 빈을 등록함
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
 	}
 
+	@Bean
+	@Profile("test")	// 테스트 프로필일 경우에만 데이터소스를 스프링 빈으로 등록함
+	public DataSource dataSource() {
+		log.info("메모리 데이터베이스 초기화");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=1");	// 임베디드(메모리) 모드로 동작하는 H2 DB 를 사용할 수 있음
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
+	}
 }
