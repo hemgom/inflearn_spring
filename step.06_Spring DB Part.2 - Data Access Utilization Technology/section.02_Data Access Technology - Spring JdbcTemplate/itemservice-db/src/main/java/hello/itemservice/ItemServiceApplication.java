32624f05803b9ba5cd8196d93c4468e438ccd183
hemgom <reborn.april23@gmail.com>
2024-02-02 13:35:53 +0900

    studied step.06-02-01

---
+++ step.06_Spring DB Part.2 - Data Access Utilization Technology/section.02_Data Access Technology - Spring JdbcTemplate/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -0,0 +1,31 @@
+package hello.itemservice;
+
+import hello.itemservice.config.*;
+import hello.itemservice.repository.ItemRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+
+
+@Import(MemoryConfig.class)	// `MemoryConfig` 를 설정 파일로 사용
+@SpringBootApplication(scanBasePackages = "hello.itemservice.web")
+public class ItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceApplication.class, args);
+	}
+
+	/**
+	 * 초기 설정 데이터 때문에 테스트 오류를 방지하기 위해 작성된 코드
+	 * 로컬에서 직접 실행 -> `local` 프로필 동작
+	 * 테스트에서 실행 -> `test` 프로필 동작
+	 */
+	@Bean
+	@Profile("local")	// 특정 프로필의 경우만 스프링 빈을 등록 -> `local`이라는 이름이 사용되는 경우만 `testDataInit`라는 스프링 빈을 등록함
+	public TestDataInit testDataInit(ItemRepository itemRepository) {
+		return new TestDataInit(itemRepository);
+	}
+
+}
---
+++ step.06_Spring DB Part.2 - Data Access Utilization Technology/section.02_Data Access Technology - Spring JdbcTemplate/itemservice-db/src/main/java/hello/itemservice/TestDataInit.java
@@ -0,0 +1,27 @@
+package hello.itemservice;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+// 애플리케이션 실행 초기 데이터 저장
+@Slf4j
+@RequiredArgsConstructor
+public class TestDataInit {
+
+    private final ItemRepository itemRepository;
+
+    /**
+     * 확인용 초기 데이터 추가
+     */
+    @EventListener(ApplicationReadyEvent.class)
+    public void initData() {
+        log.info("test data init");
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+}
