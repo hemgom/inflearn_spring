72dbda10bb9160a9372a105e035c4f7f83177816
hemgom <reborn.april23@gmail.com>
2024-02-02 22:36:31 +0900

    studied step.06-03-01

---
+++ step.06_Spring DB Part.2 - Data Access Utilization Technology/section.03_Data Access Technology - Testing/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -0,0 +1,95 @@
+package hello.itemservice.domain;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import hello.itemservice.repository.memory.MemoryItemRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// @SpringBootApplication 을 찾아서 설정으로 사용함 -> 테스트도 JdbcTemplate 을 통해 실제 DB를 호출하게 됨
+@SpringBootTest
+class ItemRepositoryTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    // 각 테스트는 서로 영향이 없어야 하기에 각 테스트 별로 테스트 종료시 저장 데이터를 제거
+    @AfterEach  // 각 테스트 실행 종료 시점에 호출
+    void afterEach() {
+        // MemoryItemRepository 의 경우 제한적으로 사용
+        // 인터페이스에 `clearStore()`가 없으므로 `MemoryItemRepository`인 경우에만 다운 캐스팅을 통해 데이터를 초기화
+        if (itemRepository instanceof MemoryItemRepository) {
+            ((MemoryItemRepository) itemRepository).clearStore();
+        }
+    }
+
+    @Test
+    void save() {
+        //given
+        Item item = new Item("itemA", 10000, 10);
+
+        //when
+        Item savedItem = itemRepository.save(item);
+
+        //then
+        Item findItem = itemRepository.findById(item.getId()).get();
+        assertThat(findItem).isEqualTo(savedItem);
+    }
+
+    @Test
+    void updateItem() {
+        //given
+        Item item = new Item("item1", 10000, 10);
+        Item savedItem = itemRepository.save(item);
+        Long itemId = savedItem.getId();
+
+        //when
+        ItemUpdateDto updateParam = new ItemUpdateDto("item2", 20000, 30);
+        itemRepository.update(itemId, updateParam);
+
+        //then
+        Item findItem = itemRepository.findById(itemId).get();
+        assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
+        assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
+        assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
+    }
+
+    @Test
+    void findItems() {
+        //given
+        Item item1 = new Item("itemA-1", 10000, 10);
+        Item item2 = new Item("itemA-2", 20000, 20);
+        Item item3 = new Item("itemB-1", 30000, 30);
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+        itemRepository.save(item3);
+
+        //둘 다 없음 검증
+        test(null, null, item1, item2, item3);
+        test("", null, item1, item2, item3);
+
+        //itemName 검증
+        test("itemA", null, item1, item2);
+        test("temA", null, item1, item2);
+        test("itemB", null, item3);
+
+        //maxPrice 검증
+        test(null, 10000, item1);
+
+        //둘 다 있음 검증
+        test("itemA", 10000, item1);
+    }
+
+    void test(String itemName, Integer maxPrice, Item... items) {
+        List<Item> result = itemRepository.findAll(new ItemSearchCond(itemName, maxPrice));
+        assertThat(result).containsExactly(items);
+    }
+}
