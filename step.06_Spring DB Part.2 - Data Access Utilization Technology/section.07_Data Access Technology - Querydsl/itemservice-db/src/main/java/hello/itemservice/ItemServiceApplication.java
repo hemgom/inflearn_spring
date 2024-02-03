44089d120db3fff664b2299d1e38d61d03a76522
hemgom <reborn.april23@gmail.com>
2024-02-04 02:10:36 +0900

    studied step.06-07-04

--- step.06_Spring DB Part.2 - Data Access Utilization Technology/section.07_Data Access Technology - Querydsl/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ step.06_Spring DB Part.2 - Data Access Utilization Technology/section.07_Data Access Technology - Querydsl/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,7 +1,6 @@
 package hello.itemservice;
 
-import hello.itemservice.config.JpaConfig;
-import hello.itemservice.config.SpringDataJpaConfig;
+import hello.itemservice.config.QuerydslConfig;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -10,10 +9,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
-
-//@Import(MemoryConfig.class)	// `MemoryConfig` 를 설정 파일로 사용
 @Slf4j
-@Import(SpringDataJpaConfig.class)
+@Import(QuerydslConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
