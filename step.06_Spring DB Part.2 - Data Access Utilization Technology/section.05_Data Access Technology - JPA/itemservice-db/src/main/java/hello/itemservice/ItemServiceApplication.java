fa49401c2a3ce12a867b4cef4a87c6b79834cbec
hemgom <reborn.april23@gmail.com>
2024-02-03 11:58:06 +0900

    studied step.06-05-05

--- step.06_Spring DB Part.2 - Data Access Utilization Technology/section.05_Data Access Technology - JPA/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ step.06_Spring DB Part.2 - Data Access Utilization Technology/section.05_Data Access Technology - JPA/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,6 +1,6 @@
 package hello.itemservice;
 
-import hello.itemservice.config.*;
+import hello.itemservice.config.JpaConfig;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -8,14 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
 
 
 //@Import(MemoryConfig.class)	// `MemoryConfig` 를 설정 파일로 사용
 @Slf4j
-@Import(MyBatisConfig.class)
+@Import(JpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
--- step.06_Spring DB Part.2 - Data Access Utilization Technology/section.05_Data Access Technology - JPA/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ step.06_Spring DB Part.2 - Data Access Utilization Technology/section.05_Data Access Technology - JPA/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -2,15 +2,25 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
+@Entity // JPA 가 사용하는 객체라는 뜻, JPA 인식표
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
 
+    // JPA 는 기본 생성자가 필수!!
     public Item() {
     }
 
