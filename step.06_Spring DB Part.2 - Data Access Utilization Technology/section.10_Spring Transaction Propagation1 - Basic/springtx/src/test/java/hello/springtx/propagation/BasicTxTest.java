4456dc1672d0a0735bf840561d9aebcf00626910
hemgom <reborn.april23@gmail.com>
2024-02-05 10:05:23 +0900

    studied step.06-10-06

--- step.06_Spring DB Part.2 - Data Access Utilization Technology/section.10_Spring Transaction Propagation1 - Basic/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ step.06_Spring DB Part.2 - Data Access Utilization Technology/section.10_Spring Transaction Propagation1 - Basic/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -1,6 +1,7 @@
 package hello.springtx.propagation;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,9 +10,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 
 import javax.sql.DataSource;
+
+import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest
@@ -117,6 +121,29 @@ public class BasicTxTest {
         // 상황4 : 외부 트랜잭션 수행 후 롤백
         log.info("외부 트랜잭션 롤백");
         txManager.rollback(outer);
-        
+
     }
+    @Test
+    void innerRollback() {
+
+        // 상황1 : 트랜잭션 동작 중
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());  // true
+
+        // 상황2 : 내부에서 트랜잭션 추가 호출 -> 즉, 내부 트랜잭션이 외부 트랜잭션에 참여
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction());  // false
+        // 상황3 : 내부 트랜잭션 수행 후 롤백
+        log.info("내부 트랜잭션 롤백");
+        txManager.rollback(inner);  // marking ... rollback-only
+
+        // 상황4 : 외부 트랜잭션 수행 후 커밋
+        log.info("외부 트랜잭션 커밋");
+        assertThatThrownBy(() -> txManager.commit(outer))
+                .isInstanceOf(UnexpectedRollbackException.class);
+
+    }
+
 }
