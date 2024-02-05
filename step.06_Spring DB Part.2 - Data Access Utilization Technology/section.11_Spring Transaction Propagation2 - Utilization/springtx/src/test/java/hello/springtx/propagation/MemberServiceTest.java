2305b815e23ab463f597a1604c4b4ea2fc382025
hemgom <reborn.april23@gmail.com>
2024-02-05 14:48:41 +0900

    studied step.06-11-02

--- step.06_Spring DB Part.2 - Data Access Utilization Technology/section.11_Spring Transaction Propagation2 - Utilization/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ step.06_Spring DB Part.2 - Data Access Utilization Technology/section.11_Spring Transaction Propagation2 - Utilization/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -37,6 +38,28 @@ class MemberServiceTest {
         //then : 모든 데이터가 정상 저장된다.
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isPresent());
+
+    }
+
+    /**
+     * Transactional 여부
+     * memberService - OFF
+     * memberRepository - ON
+     * logRepository - ON <- Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+
+        //given
+        String username = "로그예외_outerTxOff_fail";
+
+        //when
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        //then : 모든 데이터가 정상 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());    // 커밋
+        assertTrue(logRepository.find(username).isEmpty());         // 롤백 - 예외 발생
 
     }
 
