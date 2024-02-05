f015db858cb742c727ef5a7cdd699b6ed90fc2f5
hemgom <reborn.april23@gmail.com>
2024-02-05 09:24:49 +0900

    studied step.06-10-04

--- step.06_Spring DB Part.2 - Data Access Utilization Technology/section.10_Spring Transaction Propagation1 - Basic/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ step.06_Spring DB Part.2 - Data Access Utilization Technology/section.10_Spring Transaction Propagation1 - Basic/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -76,4 +76,26 @@ public class BasicTxTest {
         txManager.rollback(tx2);
     }
 
+    @Test
+    void innerCommit() {
+        
+        // 상황1 : 트랜잭션 동작 중
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());  // true
+
+        // 상황2 : 내부에서 트랜잭션 추가 호출 -> 즉, 내부 트랜잭션이 외부 트랜잭션에 참여
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction());  // false
+        // 상황3 : 내부 트랜잭션 수행 후 커밋
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);    // 내부 트랜잭션은 외부 트랜잭션에 참여했기에 커밋이 수행되지 않음
+
+        // 상황4 : 외부 트랜잭션 수행 후 커밋
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);    // 외부 트랜잭션이 물리 트랜잭션을 시작하고 커밋 함 (외부 트랜잭션이 물리 트랜잭션을 관리함)
+
+    }
+
 }
