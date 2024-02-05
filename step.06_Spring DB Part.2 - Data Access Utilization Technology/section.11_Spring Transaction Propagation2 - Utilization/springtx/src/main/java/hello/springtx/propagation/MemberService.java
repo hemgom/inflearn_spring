136b88b6c5c49a020c50f056f6a377abea89918f
hemgom <reborn.april23@gmail.com>
2024-02-05 15:01:56 +0900

    studied step.06-11-03

--- step.06_Spring DB Part.2 - Data Access Utilization Technology/section.11_Spring Transaction Propagation2 - Utilization/springtx/src/main/java/hello/springtx/propagation/MemberService.java
+++ step.06_Spring DB Part.2 - Data Access Utilization Technology/section.11_Spring Transaction Propagation2 - Utilization/springtx/src/main/java/hello/springtx/propagation/MemberService.java
@@ -3,6 +3,7 @@ package hello.springtx.propagation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -13,6 +14,7 @@ public class MemberService {
     private final LogRepository logRepository;
 
     // 회원과 DB 로그를 함께 남김
+    @Transactional
     public void joinV1(String username) {
         Member member = new Member(username);
         Log logMessage = new Log(username);
--- step.06_Spring DB Part.2 - Data Access Utilization Technology/section.11_Spring Transaction Propagation2 - Utilization/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ step.06_Spring DB Part.2 - Data Access Utilization Technology/section.11_Spring Transaction Propagation2 - Utilization/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -1,13 +1,12 @@
 package hello.springtx.propagation;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
 @SpringBootTest
@@ -60,6 +59,27 @@ class MemberServiceTest {
         //then : 모든 데이터가 정상 저장된다.
         assertTrue(memberRepository.find(username).isPresent());    // 커밋
         assertTrue(logRepository.find(username).isEmpty());         // 롤백 - 예외 발생
+
+    }
+
+    /**
+     * Transactional 여부
+     * memberService - ON
+     * memberRepository - OFF
+     * logRepository - OFF
+     */
+    @Test
+    void singleTx() {   // MemberService 시작~종료까지의 모든 로직을 하나의 트랜잭션으로 묶음
+
+        //given
+        String username = "outerTxOff_success";
+
+        //when
+        memberService.joinV1(username);
+
+        //then : 모든 데이터가 정상 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
 
     }
 
